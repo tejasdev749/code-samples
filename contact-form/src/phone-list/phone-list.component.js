a872e48e671fde11af4e23f4aa3619a963fd4db0
tejasdeogadkar <tejasdev749@gmail.com>
2026-05-08 15:43:00 +0530

    Two data binding

--- contact-form/src/phone-list/phone-list.component.js
+++ contact-form/src/phone-list/phone-list.component.js
@@ -5,19 +5,24 @@ angular.module('phoneList').component("phoneList", {
       {
         name: "Nexus S",
         snippet: "Fast just got faster with Nexus S.",
+        age: 3,
       },
       {
         name: "Motorola XOOM™ with Wi-Fi",
         snippet: "The Next, Next Generation tablet.",
+        age: 5
       },
       {
-        name: "MOTOROLA XOOM™",
+        name: "MOTOROLA A1™",
         snippet: "The Next, Next Generation tablet.",
+        age:1
       },
       {
         name: "Nokia XOOM™",
         snippet: "The Next, Next Generation tablet.",
+        age: 2
       }
     ];
+    this.orderProp = 'age'
   },
 });
