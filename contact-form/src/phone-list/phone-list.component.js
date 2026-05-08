5713e410bb3abab083470a08ba89259ce1c28357
tejasdeogadkar <tejasdev749@gmail.com>
2026-05-08 18:11:38 +0530

    dependency injection and services

--- contact-form/src/phone-list/phone-list.component.js
+++ contact-form/src/phone-list/phone-list.component.js
@@ -1,28 +1,10 @@
 angular.module('phoneList').component("phoneList", {
   templateUrl: 'phone-list/phone-list.template.html',
-  controller: function PhoneListController() {
-    this.phones = [
-      {
-        name: "Nexus S",
-        snippet: "Fast just got faster with Nexus S.",
-        age: 3,
-      },
-      {
-        name: "Motorola XOOM™ with Wi-Fi",
-        snippet: "The Next, Next Generation tablet.",
-        age: 5
-      },
-      {
-        name: "MOTOROLA A1™",
-        snippet: "The Next, Next Generation tablet.",
-        age:1
-      },
-      {
-        name: "Nokia XOOM™",
-        snippet: "The Next, Next Generation tablet.",
-        age: 2
-      }
-    ];
+  controller: ['$http', function PhoneListController($http) {
+    const that = this
+    $http.get('phones.json').then(function(response){
+      that.phones = response.data.phones.slice(0,2)
+    })
     this.orderProp = 'age'
-  },
+  }],
 });
