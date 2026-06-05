0555b6d230f301bb82eff19494c8788659e46f5d
tejasdev749 <tejasdev749@gmail.com>
2026-06-05 13:41:36 +0530

    template forms sample implementation

--- my-app/src/home/home.ts
+++ my-app/src/home/home.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'home',
-  template: `<div>Home</div>`,
+  template: `Home`,
 })
 export class Home {}
--- my-app/src/users/users.ts
+++ my-app/src/users/users.ts
@@ -1,7 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface User {
+  name: string;
+  email: string;
+}
 
 @Component({
   selector: 'users',
-  template: `<div>Users page</div>`,
+  templateUrl: './users.html',
+  imports: [FormsModule],
 })
-export class Users {}
+export class Users {
+  name = '';
+  email = '';
+  users: User[] = [];
+  submit() {
+    console.log('name:' + this.name + ' email:' + this.email);
+    this.users.push({ name: this.name, email: this.email });
+  }
+}
