4c2a81eb674842789e3a6a30df522b84e8553edd
tejasdev749 <tejasdev749@gmail.com>
2026-06-05 11:57:54 +0530

    Removed angular 1 application codebase

--- contact-form/src/app.config.js
+++
@@ -1,7 +0,0 @@
-angular.module('mainApp').config(['$routeProvider',function config($routeProvider){
-    $routeProvider.when('/phones',{
-        template: '<phone-list></phone-list>'
-    }).when('/phones/:phoneId',{
-        template: '<phone-detail></phone-detail>'
-    })
-}])
