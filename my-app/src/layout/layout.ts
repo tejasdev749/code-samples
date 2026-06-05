b285dd18fc0cc4d50f0f4475e4a1a76959c56c2e
tejasdev749 <tejasdev749@gmail.com>
2026-06-05 14:59:01 +0530

    Sample ngfor use

--- my-app/src/layout/layout.ts
+++ my-app/src/layout/layout.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'layout',
   templateUrl: './layout.html',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, NgFor],
 })
-export class Layout {}
+export class Layout {
+  items = [
+    { title: 'Users', link: 'users' },
+    { title: 'Dashboard', link: 'dashboard' },
+    { title: 'Inventory', link: 'inventory' },
+  ];
+}
